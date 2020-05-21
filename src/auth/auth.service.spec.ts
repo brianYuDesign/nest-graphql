8be47b5c89dc5e22a41c2ecc5a816a4af0ff3225
brianYuDesign <brian.yu@4idps.com>
2020-05-21 11:04:38 +0800

    Update auth.service.spec.ts

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { UsersService } from '../users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config';
+import { UserSchema } from '../users/user.schema';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        JwtModule.register({
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: 3600, // 1 hour
+          },
+        }),
+      ],
+      providers: [
+        AuthService,
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: { name: 'User', schema: UserSchema },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
