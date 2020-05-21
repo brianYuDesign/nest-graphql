b20d8dbbbf44a859df8c43842fb15188c30a9072
brianYuDesign <brian.yu@4idps.com>
2020-05-21 11:04:57 +0800

    Update users.service.spec.ts

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const userModel = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getModelToken('User'), useValue: userModel },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
