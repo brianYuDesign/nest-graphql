b4528ed4637db5464d7336f7ba342e1a4e87a0b2
brianYuDesign <brian.yu@4idps.com>
2020-06-22 21:52:23 +0800

    Update gql-auth.guard.ts

--- src/auth/gql-auth.guard.ts
+++ src/auth/gql-auth.guard.ts
@@ -7,7 +7,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
-    console.log(ctx.getContext().req);
     return ctx.getContext().req;
   }
 }
